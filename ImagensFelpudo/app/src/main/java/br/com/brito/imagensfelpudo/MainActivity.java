b6ce8c7c235d4a84cbaf00452ea22b98ef5fe3b5
Carlos Bruno <cbcarlos08@gmail.com>
2019-02-20 17:51:32 -0400

    Aula 82. Configurando Listas e Layouts

--- ImagensFelpudo/app/src/main/java/br/com/brito/imagensfelpudo/MainActivity.java
+++ ImagensFelpudo/app/src/main/java/br/com/brito/imagensfelpudo/MainActivity.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] listaNomes     = {"Felpudo","Fofura", "Lesmo", "Bugado", "Uruca", "Racing", "iOS", "Android", "Realidade Aumentada", "SoundFX", "3D Studio Max", "Games" };
+    int[]    listaIcones    = {R.drawable.felpudo,R.drawable.fofura,R.drawable.lesmo,R.drawable.bugado,R.drawable.uruca,R.drawable.carrinho,
+            R.drawable.ios,R.drawable.android,R.drawable.realidade_aumentada,R.drawable.sound_fx,R.drawable.max,R.drawable.games};
+    String[] listaDescricao = {"Felpudo","Fofura", "Lesmo", "Bugado", "Uruca", "Racing", "iOS", "Android", "Realidade Aumentada", "SoundFX", "3D Studio Max", "Games" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- ImagensFelpudo/app/src/main/java/br/com/brito/imagensfelpudo/SegundaTela.java
+++ ImagensFelpudo/app/src/main/java/br/com/brito/imagensfelpudo/SegundaTela.java
@@ -2,16 +2,21 @@ package br.com.brito.imagensfelpudo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegundaTela extends AppCompatActivity {
-
+    TextView meuTexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_tela);
         Bundle extras = getIntent().getExtras();
+
+        //meuTexto = findViewById( R.id.meuTexto );
+
         if( extras != null ){
-            //String texto
+            String texto = extras.getString( "nome" );
+           // meuTexto.setText( texto );
         }
     }
 }
