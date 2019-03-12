fb8333cb3f7b9de65111223c4efc34bb00b492ad
Carlos Bruno <cbcarlos08@gmail.com>
2019-03-12 12:14:43 -0400

    Aula 126. Ativando o HOme Button

--- MatDesign/app/src/main/java/brito/com/matdesign/MainActivity.java
+++ MatDesign/app/src/main/java/brito/com/matdesign/MainActivity.java
@@ -3,6 +3,9 @@ package brito.com.matdesign;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById( R.id.minhaToolbar );
         setSupportActionBar( toolbar );
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.meu_menu, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_contato:
+                Toast.makeText(this, "Clicou contato", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_favorito:
+                Toast.makeText(this, "Clicou favorito", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_home:
+                Toast.makeText(this, "Clicou home", Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                Toast.makeText(this, "Clicou Backbutton", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
