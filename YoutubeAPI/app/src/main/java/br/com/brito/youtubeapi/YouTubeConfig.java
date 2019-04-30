f9eab948fab45d91c49aa58870a8f89ce382f3f0
Carlos Bruno <cbcarlos08@gmail.com>
2019-04-30 07:01:37 -0400

    Aula 159 -  YoutubeAPI - Configuração

---
+++ YoutubeAPI/app/src/main/java/br/com/brito/youtubeapi/YouTubeConfig.java
@@ -0,0 +1,12 @@
+package br.com.brito.youtubeapi;
+
+public class YouTubeConfig {
+    public static String API_KEY = "AIzaSyALJORdJO18X4qzmgGmmLzVYqXg7hvA6Es";
+    //SHA1: 06:0A:CE:73:7A:E5:21:9F:0F:D0:FC:A4:0D:B7:3E:98:BB:5D:9E:5C
+    public YouTubeConfig(){
+
+    }
+    public static String getAPI_KEY(){
+        return API_KEY;
+    }
+}
