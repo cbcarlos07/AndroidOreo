586d61df8cc85e0d04098ac6ee4ce112c29039de
Carlos Bruno <cbcarlos08@gmail.com>
2019-04-30 12:16:15 -0400

    Aula 160 -  YoutubeAPI - YouTubePlayerView

--- YoutubeAPI/app/src/main/java/br/com/brito/youtubeapi/MainActivity.java
+++ YoutubeAPI/app/src/main/java/br/com/brito/youtubeapi/MainActivity.java
@@ -3,12 +3,32 @@ package br.com.brito.youtubeapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-     //AIzaSyALJORdJO18X4qzmgGmmLzVYqXg7hvA6Es
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+     YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener ytListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        YouTubeConfig.getAPI_KEY();
+
+        youTubePlayerView = findViewById( R.id.meuPlayer );
+        ytListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo( "mhmaLQ1TFa0" );
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        youTubePlayerView.initialize( YouTubeConfig.getAPI_KEY(), ytListener );
     }
 }
