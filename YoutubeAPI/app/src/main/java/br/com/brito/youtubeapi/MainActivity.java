eacde66397f00d151db018e750c3e7db249749f4
Carlos Bruno <cbcarlos08@gmail.com>
2019-05-03 06:45:58 -0400

    Aula 163 -  Projeto finalizado - Videos do YouTube

--- YoutubeAPI/app/src/main/java/br/com/brito/youtubeapi/MainActivity.java
+++ YoutubeAPI/app/src/main/java/br/com/brito/youtubeapi/MainActivity.java
@@ -14,17 +14,19 @@ public class MainActivity extends YouTubeBaseActivity {
     YouTubePlayerView youTubePlayerView;
     YouTubePlayer.OnInitializedListener ytListener;
     ImageView image;
+    String youtubeID = "LwU8TELuVh4";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //https://img.youtube.com/vi/6lZp4to_7ZI/hqdefault.jpg
         youTubePlayerView = findViewById( R.id.meuPlayer );
         image = findViewById( R.id.imageView );
         ytListener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo( "pBOo8IcYLKY&t" );
+                youTubePlayer.loadVideo( youtubeID );
+               // youTubePlayer.cueVideo( youtubeID );
             }
 
             @Override
@@ -34,7 +36,7 @@ public class MainActivity extends YouTubeBaseActivity {
         };
 
         youTubePlayerView.initialize( YouTubeConfig.getAPI_KEY(), ytListener );
-        String caminhoImagem = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvndm8RETCt8HUmi0t7AyMtzeP2fOLVPtrYljFV8hKPn33Md_n";
+        String caminhoImagem = "https://img.youtube.com/vi/"+youtubeID+"/hqdefault.jpg";
         Picasso.with( getApplicationContext() ).load( caminhoImagem ).into( image );
     }
 }
