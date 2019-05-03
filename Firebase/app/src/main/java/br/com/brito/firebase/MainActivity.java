3f6ab2570c510bb8cac820612788bf0b91cdea19
Carlos Bruno <cbcarlos08@gmail.com>
2019-05-03 16:24:45 -0400

    169 - Escrevendo multiplos dados

--- Firebase/app/src/main/java/br/com/brito/firebase/MainActivity.java
+++ Firebase/app/src/main/java/br/com/brito/firebase/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.brito.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,7 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseDatabase database= FirebaseDatabase.getInstance();
+
+        //DatabaseReference myRef = database.getReference("nomes/Daniel/cidade");
         DatabaseReference myRef = database.getReference("nomes");
-        myRef.setValue( "Hello World" );
+        //myRef.setValue( "Hello World" );
+        myRef.child("Daniel").child("cidade").setValue("teste1");
+        Log.d( "meuLog", "firebase" );
     }
 }
