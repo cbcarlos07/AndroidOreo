529617aa6dc28760ed034db262bcbc5335c9ceeb
Carlos Bruno <cbcarlos08@gmail.com>
2019-05-03 14:32:43 -0400

    167 - Inserindo no banco de dados

--- Firebase/app/src/main/java/br/com/brito/firebase/MainActivity.java
+++ Firebase/app/src/main/java/br/com/brito/firebase/MainActivity.java
@@ -3,11 +3,17 @@ package br.com.brito.firebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database= FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("nomes");
+        myRef.setValue( "Hello World" );
     }
 }
