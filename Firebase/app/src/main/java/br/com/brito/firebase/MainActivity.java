0710d8d138a9e9e6ed37455bb4dba7956ffb3822
Carlos Bruno <cbcarlos08@gmail.com>
2019-05-07 11:52:24 -0400

    173 - Fazendo login e logout

--- Firebase/app/src/main/java/br/com/brito/firebase/MainActivity.java
+++ Firebase/app/src/main/java/br/com/brito/firebase/MainActivity.java
@@ -5,7 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,7 +24,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,5 +92,71 @@ public class MainActivity extends AppCompatActivity {
         };
         myRef.addChildEventListener( nomes );
         myRef.push().setValue( new Pessoa("teste","teste", "teste", "teste") );*/
+
+        mAuth = FirebaseAuth.getInstance();
+      /*  mAuth.createUserWithEmailAndPassword( "teste@teste.com", "12345678" )
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        FirebaseUser currentUser = mAuth.getCurrentUser();
+                        if( currentUser != null ){
+                            //ir para a proxima tela
+                            Log.d("log", "Ir para a proxima tela");
+                        }else{
+                            Toast.makeText(MainActivity.this, "Falha na criação de usuário", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });*/
+     /* mAuth.signInWithEmailAndPassword("teste@teste.com", "12345678")
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        FirebaseUser currentUser = mAuth.getCurrentUser();
+                        if( currentUser != null ){
+                            //ir para a proxima tela
+                            Log.d("log", "Logado com sucesso");
+                        }else{
+                            Toast.makeText(MainActivity.this, "Falha na autenticação do usuário", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if( currentUser != null ){
+            //ir para a proxima tela
+            //Log.d("log", "Ir para a proxima tela");
+            Log.d("log", "Logado com sucesso");
+        }
+    }
+
+    public void logout(View view){
+        //FirebaseAuth.getInstance().signOut();
+        mAuth.signOut();
+        Toast.makeText(MainActivity.this, "Deslotou", Toast.LENGTH_SHORT).show();
+    }
+    public void login( View view ){
+        mAuth.signInWithEmailAndPassword("teste@teste.com", "12345678")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser currentUser = mAuth.getCurrentUser();
+                            if( currentUser != null ){
+                                //ir para a proxima tela
+                                Log.d("log", "btn Logado com sucesso");
+                            }else{
+                                Toast.makeText(MainActivity.this, "Falha na autenticação do usuário", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
     }
 }
