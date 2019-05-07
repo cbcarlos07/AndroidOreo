7f718441762a162ced1de7b9c948b3d20c871cdf
Carlos Bruno <cbcarlos08@gmail.com>
2019-05-07 15:26:18 -0400

    Aula 175 - Tela Login

--- TwitterCurso/app/src/main/java/br/com/brito/twittercurso/LoginActivity.java
+++ TwitterCurso/app/src/main/java/br/com/brito/twittercurso/LoginActivity.java
@@ -1,13 +1,78 @@
 package br.com.brito.twittercurso;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    
+    private FirebaseAuth mAuth;
+    private EditText editEmail;
+    private EditText editSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editEmail = findViewById( R.id.campoEmail );
+        editSenha = findViewById( R.id.campoSenha );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI( currentUser );
+    }
+
+    private void updateUI( FirebaseUser user ){
+        if( user != null ){
+            //passar para proxima tela
+        }
+    }
+
+    public void login(View view){
+        String email = editEmail.getText().toString().trim();
+        String senha = editSenha.getText().toString().trim();
+
+        if( email.equals("") ){
+            editEmail.setError( "Preencha este campo!" );
+            editEmail.requestFocus();
+            return;
+        }
+
+        if( senha.equals("") ){
+            editSenha.setError( "Preencha este campo" );
+            editSenha.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword( email, senha ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if( task.isSuccessful() ){
+                    updateUI( mAuth.getCurrentUser() );
+                }else{
+                    Toast.makeText(LoginActivity.this, "Usuário ou senha incorretos!", Toast.LENGTH_SHORT).show();
+                    updateUI( null );
+                }
+            }
+        });
+
+
+    }
+
+    public void cadastro( View view){
+
     }
 }
