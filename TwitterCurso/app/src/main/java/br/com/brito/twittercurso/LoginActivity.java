49895b81bc911992e4d95260b321f549781e2879
Carlos Bruno <cbcarlos08@gmail.com>
2019-05-07 14:51:14 -0400

    Aula 175 - Tela Login

---
+++ TwitterCurso/app/src/main/java/br/com/brito/twittercurso/LoginActivity.java
@@ -0,0 +1,13 @@
+package br.com.brito.twittercurso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+    }
+}
