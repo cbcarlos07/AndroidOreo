c832b2882dfeb7f0a12044bf8ea77d6b86daaf85
Carlos Bruno <cbcarlos08@gmail.com>
2019-05-13 06:55:56 -0400

    Aula 191 - Fundo Infinito

---
+++ FlappyBird/core/src/br/com/brito/flappybird/Background.java
@@ -0,0 +1,33 @@
+package br.com.brito.flappybird;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Background {
+    private Texture img;
+
+    private float x1, x2;
+
+    public Background(){
+        img = new Texture( "fundo.png" );
+
+        x1 = 0;
+        x2 = 1000;
+    }
+
+    public void update( float delta ){
+        float SPEED = -400;
+        x1 += SPEED * delta;
+        x2 += SPEED * delta;
+        if( x1 <= - 1000 ){
+            x1 = 0;
+            x2 = 1000;
+        }
+    }
+
+    public void draw(SpriteBatch batch){
+        batch.draw( img, x1, 0, 1000, 1700 );
+        batch.draw( img, x2, 0, 1000, 1700);
+    }
+
+}
--- FlappyBird/core/src/br/com/brito/flappybird/MainClass.java
+++ FlappyBird/core/src/br/com/brito/flappybird/MainClass.java
@@ -5,29 +5,41 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Viewport viewport;
+	private SpriteBatch batch;
+	private Background back;
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		viewport = new FitViewport( 1000, 1700 );
+		batch =  new SpriteBatch();
+		back  = new Background();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	    float delta = Gdx.graphics.getDeltaTime();
+	    back.update( delta );
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.setProjectionMatrix( viewport.getCamera().combined );
 		batch.begin();
-		batch.draw(img, 0, 0);
+		back.draw( batch );
 		batch.end();
 	}
-	
+
+	@Override
+	public void resize(int width, int height) {
+		viewport.update( width, height, true );
+
+	}
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
